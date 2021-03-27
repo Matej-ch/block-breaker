6f04ab9355aa9062eb45ff520ac89e176c0d2f61
Matej <matejchalachan@gmail.com>
2021-03-27 17:41:49 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,88 @@
+
+const grid = document.getElementById('grid');
+
+const blockWidth = 100;
+const blockHeight = 20;
+const boardWidth = 600;
+const userStart = [230,10];
+const ballStart = [270,40];
+let ballCurrentPosition = ballStart;
+let currentPosition = userStart;
+
+class Block {
+    constructor(xAxis,yAxis) {
+        this.bottomLeft = [xAxis,yAxis];
+        this.bottomRight = [xAxis + blockWidth,yAxis];
+        this.topLeft = [xAxis,yAxis + blockHeight];
+        this.topRight = [xAxis + blockWidth,yAxis + blockHeight];
+    }
+}
+
+const blocks = [
+    new Block(10,270),
+    new Block(120,270),
+    new Block(230,270),
+    new Block(340,270),
+    new Block(450,270),
+
+    new Block(40,240),
+    new Block(150,240),
+    new Block(260,240),
+    new Block(370,240),
+    new Block(480,240),
+
+    new Block(10,210),
+    new Block(120,210),
+    new Block(230,210),
+    new Block(340,210),
+    new Block(450,210),
+]
+
+function addBlock() {
+
+    for(let i =0;i < blocks.length;i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.left = blocks[i].bottomLeft[0] + 'px';
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+        grid.appendChild(block);
+    }
+}
+
+addBlock();
+
+const user = document.createElement('div');
+user.classList.add('user');
+drawUser();
+grid.appendChild(user);
+
+function moveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if(currentPosition[0] > 0) {
+                currentPosition[0] -= 10;
+                drawUser()
+            }
+            break
+        case 'ArrowRight':
+            if(currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10;
+                drawUser()
+            }
+            break
+    }
+}
+
+document.addEventListener('keydown',moveUser);
+
+
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px';
+    user.style.bottom = currentPosition[1] + 'px';
+}
+
+const ball = document.createElement('div')
+ball.classList.add('ball');
+ball.style.left = ballCurrentPosition[0] + 'px';
+ball.style.bottom = ballCurrentPosition[1] + 'px';
+grid.appendChild(ball);
